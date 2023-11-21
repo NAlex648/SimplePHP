586b14f97c2020669e9e4cad9e51c8a303507ebe
NAlex648 <118040735+NAlex648@users.noreply.github.com>
2023-11-21 10:09:51 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,40 @@
+<?php
+session_start();
+include_once 'config.php';
+
+// Checking if the user is already logged in
+if (isset($_SESSION['user_id'])) {
+    header("Location: index.php");
+    exit();
+}
+
+// Checking if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // User input
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Password Hashing
+    $hashed_password = md5($password);
+
+    // Checking user's credentials
+    $sql = "SELECT * FROM users WHERE username='$username' AND password='$hashed_password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        $_SESSION['user_id'] = $row['id'];
+        $_SESSION['username'] = $username;
+
+        // Redirect to home page after successful login
+        header("Location: home.php");
+        exit();
+    } else {
+        $error_message = "Invalid username or password.";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +44,8 @@
 </head>
 <body>
     <h1>Login</h1>
-    <form action="login_process.php" method="post">
+    <?php if (isset($error_message)) echo '<p>' . $error_message . '</p>'; ?>
+    <form action="login.php" method="post">
         <label for="username">Username:</label>
         <input type="text" name="username" required><br>
 
