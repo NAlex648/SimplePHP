5b7b716c099ddc73373251a661fccf0c8732c9df
Alex648 <118040735+NAlex648@users.noreply.github.com>
2023-12-15 01:35:35 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,11 +2,9 @@
 $servername = "localhost";
 $username = "root";  // Default MySQL username for XAMPP
 $password = "";      // Default MySQL password for XAMPP (empty by default)
-$database = "simple_php_database";  // The name of my database I'm using in XAMPP, you may change the value to your database name you're using
-
+$database = "simple_php_database";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
-
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
