d5c8a8b83c29b91699f0318493acc58452168073
Alex648 <118040735+NAlex648@users.noreply.github.com>
2023-12-15 01:38:22 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,58 +1,80 @@
 <?php
 session_start();
 include_once 'config.php';
-
-// Checking if the user is already logged in
-if (isset($_SESSION['user_id'])) {
-    header("Location: index.php");
+// XSS Prevention
+function sanitizeInput($input) {
+    return htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+}
+// CSRF Prevention
+function generateCSRFToken() {
+    return bin2hex(random_bytes(32));
+}
+function validateCSRFToken($token) {
+    return isset($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $token);
+}
+// Check if CSRF token is already set, if not, generate and set it
+if (!isset($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = generateCSRFToken();
+}
+// Checking if the user is already logged in to prevent pararel session
+if (isset($_SESSION['username'])) {
+    header("Location: home.php");
     exit();
 }
-
-// Checking if the form is submitted
+$csrfInput = '<input type="hidden" name="csrf_token" value="' . $_SESSION['csrf_token'] . '">';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // User input
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    // Password Hashing
-    $hashed_password = md5($password);
-
-    // Checking user's credentials
-    $sql = "SELECT * FROM users WHERE username='$username' AND password='$hashed_password'";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows == 1) {
-        $row = $result->fetch_assoc();
-        $_SESSION['user_id'] = $row['id'];
-        $_SESSION['username'] = $username;
-
-        // Redirect to home page after successful login
-        header("Location: home.php");
-        exit();
+    $username = sanitizeInput($_POST['username']);
+    $password = sanitizeInput($_POST['password']);
+    // CSRF token validation
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['csrf_token']) && validateCSRFToken($_POST['csrf_token'])) {
+        $sql = "SELECT username, password FROM users WHERE username = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $stmt->store_result();
+        if ($stmt->num_rows == 1) {
+            $stmt->bind_result($db_username, $db_password);
+            $stmt->fetch();
+            if (hash('sha256', $password) === $db_password) {
+                $_SESSION['username'] = $db_username;
+                // Regenerate session ID for Cookie Session Hijack Prevention
+                session_regenerate_id(true);
+                header("Location: home.php");
+                exit();
+            } else {
+                $error_message = "Invalid username or password.";
+            }
+        } else {
+            $error_message = "Invalid username or password.";
+        }
+        $stmt->close();
     } else {
-        $error_message = "Invalid username or password.";
+        $error_message = "CSRF Token validation failed.";
     }
+    // Generate a new CSRF token for the next form submission
+    $_SESSION['csrf_token'] = generateCSRFToken();
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
     <title>Login</title>
 </head>
 <body>
-    <h1>Login</h1>
-    <?php if (isset($error_message)) echo '<p>' . $error_message . '</p>'; ?>
-    <form action="login.php" method="post">
-        <label for="username">Username:</label>
-        <input type="text" name="username" required><br>
-
-        <label for="password">Password:</label>
-        <input type="password" name="password" required><br>
-
-        <input type="submit" value="Login">
-    </form>
+    <div class="container">
+        <h1>Login</h1>
+        <form action="login.php" method="post">
+            <?php if (isset($csrfInput)) echo $csrfInput; ?>
+            <label for="username">Username:</label>
+            <input type="text" name="username" required><br>
+            <label for="password">Password:</label>
+            <input type="password" name="password" required><br>
+            <input type="submit" value="Login">
+        </form>
+        <div class="message"></div>
+    </div>
 </body>
 </html>
