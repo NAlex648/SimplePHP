396cc5d56e0358dfa25b39e10c93c824f7d81ae4
Alex648 <118040735+NAlex648@users.noreply.github.com>
2023-12-15 01:39:37 +0700

    Update simple_sql_database.sql

--- simple_sql_database.sql
+++ simple_sql_database.sql
@@ -6,33 +6,30 @@
 -- Generation Time: Nov 21, 2023 at 04:00 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `simple_php_app`
 --
-
+create database simple_php_database;
+use simple_php_database;
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `users`
 --
-
 CREATE TABLE `users` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `users` (`username`, `password`) VALUES
+('username', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),
+('user123', 'ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f');
 COMMIT;
-
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
