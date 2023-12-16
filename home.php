9b2a6475cda23ee84ada58fb45c9245d29aecf4d
Alex648 <118040735+NAlex648@users.noreply.github.com>
2023-12-16 18:17:06 +0700

    Update home.php
    Added artwork submition

--- home.php
+++ home.php
@@ -14,14 +14,32 @@ function validateCSRFToken($token) {
 }
 // Can't go to home.php if not logged in
 if (!isset($_SESSION['username'])) {
-    //goes back to index for log in
+    // Goes back to index for log in
     header("Location: index.php");
     exit();
 }
 $username = $_SESSION['username'];
 // CSRF token validation
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['csrf_token']) && validateCSRFToken($_POST['csrf_token'])) {
-    // Your logic for processing forms or other actions
+    // Check artwork submission form
+    if (isset($_FILES['artwork']) && $_FILES['artwork']['error'] === UPLOAD_ERR_OK) {
+        // Check file type
+        $allowedExtensions = ['png', 'jpeg', 'jpg'];
+        $fileExtension = strtolower(pathinfo($_FILES['artwork']['name'], PATHINFO_EXTENSION));
+        if (in_array($fileExtension, $allowedExtensions)) {
+            // Move the uploaded file to a secure location
+            $uploadDirectory = 'uploads/';
+            $newFileName = $username . '_' . time() . '.' . $fileExtension;
+            $uploadPath = $uploadDirectory . $newFileName;
+            if (move_uploaded_file($_FILES['artwork']['tmp_name'], $uploadPath)) {
+                echo 'Artwork successfully uploaded!';
+            } else {
+                echo 'Error uploading artwork.';
+            }
+        } else {
+            echo 'Invalid file type. Please upload a .png, .jpeg, or .jpg file.';
+        }
+    }
 } else {
     $error_message = "CSRF Token validation failed.";
 }
@@ -37,6 +55,12 @@ $_SESSION['csrf_token'] = generateCSRFToken();
 </head>
 <body>
     <h1><?php echo "Hello $username! Welcome to the home page"?></h1>
+    <form action="home.php" method="post" enctype="multipart/form-data">
+        <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
+        <label for="artwork">Upload Artwork:</label>
+        <input type="file" name="artwork" accept=".png, .jpeg, .jpg" required>
+        <input type="submit" value="Submit Artwork">
+    </form>
     <?php
     // For logging out
     echo '<a href="logout.php">Logout</a>';
